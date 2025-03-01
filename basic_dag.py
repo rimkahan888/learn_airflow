920f88d27eb0a25192fec19fc2879f7797629bdf
Johan Rimkahan <115785092+rimkahan888@users.noreply.github.com>
2025-03-02 12:16:41 +1300

    Update basic_dag.py

--- basic_dag.py
+++ basic_dag.py
@@ -13,6 +13,28 @@ default_args = {
 
 # Define the DAG
 with DAG(
-    'basic_example_dag',
+    'basic_example_dag',                    # DAG ID 
     default_args=default_args,
+    description='A Simple Airflow DAG',        # Run daily (@daily)
+    schedule_interval=timedelta(days=1),        # Skip historical runs
+    catchup=False,
+) as dag:
+
+    #Task 1: Print "Hello, World'! using Bash
+    task_hello_bash=BashOperator(
+        task_id='hello_bash'
+        bash_command='echo "Hello, World from Bash!"'
+
+    #Task 2: Print "Hello from Python!" using Python
+    def print_hello_python():
+        print("Hello from Python!)
+
+    task_hello_python = PythonOperator(
+        task_id ='hello_python',
+        python_callable = print_hello_python
+
+    PythonOperator
+    BashOperator
+
+    
     
