0110ca1c72520507d893028742bbe26803221cbb
Johan Rimkahan <115785092+rimkahan888@users.noreply.github.com>
2025-03-01 22:40:57 +0000

    basic_dag

---
+++ basic_dag.py
@@ -0,0 +1,13 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airlfow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+# Default argumetns for the DAG
+default_args = {
+    'owner' : 'airflow',
+    'start_date' : datetime(2023, 11, 1),
+    'retries' : 1,
+    'retry_delay' : timedelta(minutes=5),
+}
+
